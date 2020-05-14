876e7e67c08051237628116edaa77cfdaec41fb4
mkuchimanchi82 <63679826+mkuchimanchi82@users.noreply.github.com>
2020-05-14 16:42:22 -0500

    Update HellocdApplication.java

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellocdApplication {
 
+	@RequestMapping("/hellocd")
+	String home() {
+		return "Hello CD ! Wohhooooo ";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HellocdApplication.class, args);
 	}
-
 }
